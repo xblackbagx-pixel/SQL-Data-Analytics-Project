3e05a977249a6f19520ba4d8904abae540ca356f
xblackbagx-pixel <xblackbagx@gmail.com>
2026-05-24 15:59:53 -0400

    Add additional metrics to Business_Report.sql

--- Dimension Exploration/Business_Report.sql
+++ Dimension Exploration/Business_Report.sql
@@ -31,3 +31,11 @@ SELECT COUNT(DISTINCT customer_key) AS total_customers FROM gold.fact_sales
 SELECT 'Total Sales' AS measure_name, SUM(Sales_amount) AS measure_value FROM gold.fact_sales
 UNION ALL 
 SELECT 'Total Quantity', SUM(quantity) FROM gold.fact_sales
+UNION ALL
+SELECT 'Average Price', AVG(price) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Nr. Order', COUNT(DISTINCT order_number) FROM gold.fact_sales
+UNION ALL 
+SELECT 'Total Nr. Products', COUNT(product_name) FROM gold.dim_products
+UNION ALL
+SELECT 'Total Nr. Customers', COUNT(Customer_key) FROM gold.dim_customers
